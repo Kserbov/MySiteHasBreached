bb024c9e64c7a6b9f5331b3dfa4f6ab9379f17a0
Kserbov <108228131+Kserbov@users.noreply.github.com>
2022-07-03 15:24:44 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
 function function1(){
 	if(input1.value == 'vimeworld.ru'){
 		leaked.textContent = 'Oh no! 5m users data of this site has leaked - https://disk.yandex.ru/d/kgBwHDGQsfQiFg'
-	}
-	else if(input1.value == 'wattpad.com'){
+	}else if(input1.value == 'wattpad.com'){
 		leaked.textContent = 'Oh no! 271m users data of this site has leaked - https://gofile.io/d/PaQfhs'
-	}
-	else if(input1.value == 'mail.ru'){
+	}else if(input1.value == 'mail.ru'){
 		leaked.textContent = 'Oh no! 4m users data of this site has leaked - https://dfiles.eu/files/vcq9une6u'
-	}
-	else if(input1.value == 'yandex.ru'){
+	}else if(input1.value == 'yandex.ru'){
 		leaked.textContent = 'Oh no! 1m users data of this site has leaked - https://dfiles.eu/files/fqs8odhlg'
-	}
-	else if(input1.value == 'gamigo.com'){
-		leaked.textContent = 'Oh no! 8m users data of this site has leaked - https://ImNotFindDownloadLeakedLink'
-	}
-	else if(input1.value == 'animoto.com'){
+	}else if(input1.value == 'animoto.com'){
 		leaked.textContent = 'Oh no! 22m users data of this site has leaked - https://breached.to/Thread-Animoto-Database-Leaked-Download'
-	}
-	else if(input1.value == 'dropbox.com'){
-		leaked.textContent = 'Oh no! 68m users data of this site has leaked - https://ImNotFindedLeakedLink'
-	}
-	else if(input1.value == 'badoo.com'){
+	}else if(input1.value == 'badoo.com'){
 		leaked.textContent = 'Oh no! 112m users data of this site has leaked - https://spyhackerz.rusff.me/viewtopic.php?id=153'
-	}
-	else if(input1.value == 'myspace.com'){
-		leaked.textContent = 'Oh no! 112m users data of this site has leaked - https://ImNotFindedLeakedLink'
-	}
-	else{
+	}else if(input1.value == 'strongholdkingdoms.com'){
+		leaked.textContent = 'Oh no! 5m users data of this site has leaked - https://www.nulled.to/topic/943533-stronghold-kingdoms-2018-database-leaked-download/'
+	}else if(input1.value == 'leet.cc'){
+		leaked.textContent = 'Oh no! 6m users data of this site has leaked - https://zero-allians.ru/threads/leet-cc-database-leaked-august-2016-free-download.1621/'
+	}else{
 		leaked.textContent = 'Good news! users data of this site not leaked'
 	}
 }
