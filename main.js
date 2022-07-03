cb3002fbe75abd3cd3cdc4e7c2a9947274ff8fe3
Kserbov <108228131+Kserbov@users.noreply.github.com>
2022-07-03 13:49:46 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+function function1(){
+	if(input1.value == 'vimeworld.ru'){
+		leaked.textContent = 'Oh no! 5m users data of this site has leaked - https://disk.yandex.ru/d/kgBwHDGQsfQiFg'
+	}
+	else if(input1.value == 'wattpad.com'){
+		leaked.textContent = 'Oh no! 271m users data of this site has leaked - https://gofile.io/d/PaQfhs'
+	}
+	else if(input1.value == 'mail.ru'){
+		leaked.textContent = 'Oh no! 4m users data of this site has leaked - https://dfiles.eu/files/vcq9une6u'
+	}
+	else if(input1.value == 'yandex.ru'){
+		leaked.textContent = 'Oh no! 1m users data of this site has leaked - https://dfiles.eu/files/fqs8odhlg'
+	}
+	else if(input1.value == 'gamigo.com'){
+		leaked.textContent = 'Oh no! 8m users data of this site has leaked - https://ImNotFindDownloadLeakedLink'
+	}
+	else if(input1.value == 'animoto.com'){
+		leaked.textContent = 'Oh no! 22m users data of this site has leaked - https://breached.to/Thread-Animoto-Database-Leaked-Download'
+	}
+	else if(input1.value == 'dropbox.com'){
+		leaked.textContent = 'Oh no! 68m users data of this site has leaked - https://ImNotFindedLeakedLink'
+	}
+	else if(input1.value == 'badoo.com'){
+		leaked.textContent = 'Oh no! 112m users data of this site has leaked - https://spyhackerz.rusff.me/viewtopic.php?id=153'
+	}
+	else if(input1.value == 'myspace.com'){
+		leaked.textContent = 'Oh no! 112m users data of this site has leaked - https://ImNotFindedLeakedLink'
+	}
+	else{
+		leaked.textContent = 'Good news! users data of this site not leaked'
+	}
+}
