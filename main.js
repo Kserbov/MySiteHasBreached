0e14b8f0c62cec31563a9701b1900d2804593105
Kirill Rozhnov <108228131+Kserbov@users.noreply.github.com>
2022-07-10 23:13:38 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function function1(){
 	if(input1.value == 'vimeworld.ru'){
 		leaked.textContent = 'Oh no! 5m users data of this site has leaked - https://disk.yandex.ru/d/kgBwHDGQsfQiFg'
 	}else if(input1.value == 'wattpad.com'){
-		leaked.textContent = 'Oh no! 271m users data of this site has leaked - https://gofile.io/d/PaQfhs'
+		leaked.textContent = 'Oh no! 271m users data of this site has leaked  - https://gofile.io/d/PaQfhs'
 	}else if(input1.value == 'mail.ru'){
 		leaked.textContent = 'Oh no! 4m users data of this site has leaked - https://dfiles.eu/files/vcq9une6u'
 	}else if(input1.value == 'yandex.ru'){
@@ -29,6 +29,12 @@ function function1(){
 		leaked.textContent = 'Oh no! 40m users data of this site has leaked - https://zone1b.com/threads/dehashed-fling-com-database-combo-leaked-march-2011-free-download.750/'	
 	}else if(input1.value == 'r2Games.com'){
 		leaked.textContent = 'Oh no! 3m users data of this site has leaked - https://breached.to/Thread-R2Games-2019-forum-breach-Database-Leaked-Download'	
+	}else if(input1.value == 'gamesalad.com'){
+		leaked.textContent = 'Oh no! 1m users data of this site has leaked - https://breached.to/Thread-GameSalad-Database-Leaked-Download'	
+	}else if(input1.value == 'myspace.com'){
+		leaked.textContent = 'Oh no! 427m users data of this site has leaked - https://sudonull.com/post/23296-Torrent-with-427-million-MySpace-passwords'	
+	}else if(input1.value == 'aptoide.com'){
+		leaked.textContent = 'Oh no! 39m users data of this site has leaked - https://github.com/hacxx-underground/Files/blob/main/(39mil)%20Aptoide.com%20Database%20Leaked%20April%202020%20-%20Free%20Download'	
 	}else{
 		leaked.textContent = 'Good news! users data of this site not leaked'
 	}
